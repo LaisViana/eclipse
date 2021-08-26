56cf1b85c4d920f72541a9c20c6146274ce2c321
LaisViana <86385439+LaisViana@users.noreply.github.com>
2021-08-26 18:03:30 -0300

    adicionei arquivos por uploud testar

---
+++ integrar/src/br/com/lais/git/hellogit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.lais.git;
+
+/**
+ * @author PATRICIAVIANACOSTA
+ *
+ */
+public class hellogit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Só exemplo");
+
+	}
+
+}
